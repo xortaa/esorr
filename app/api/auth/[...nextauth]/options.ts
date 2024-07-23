63aace4bac80c76db825951b30c4162e5ca494af
xortaa <kercwinpaul@gmail.com>
2024-07-23 22:20:43 +0800

    insert image from google to user.image. cross credentials ang google login

--- app/api/auth/[...nextauth]/options.ts
+++ app/api/auth/[...nextauth]/options.ts
@@ -5,11 +5,20 @@ import dbConnect from "@/db/mongodb";
 import User from "@/models/user";
 import bcrypt from "bcryptjs";
 
+declare module "next-auth" {
+  interface Profile {
+    picture?: string;
+  }
+}
+
 export const options: NextAuthOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      authorization: {
+        params: { access_type: "offline", prompt: "consent" },
+      },
     }),
     CredentialsProvider({
       name: "Credentials",
@@ -18,21 +27,21 @@ export const options: NextAuthOptions = {
         password: { label: "Password", type: "enter", placeholder: "Enter password" },
       },
       async authorize(credentials): Promise<any> {
+        if (!credentials) return null;
+
         await dbConnect();
 
         const userFound = await User.findOne({
           email: credentials.email,
         }).select("+password");
 
-        if (!userFound) {
+        if (userFound) {
+          const passwordMatch = await bcrypt.compare(credentials!.password, userFound.password);
+          if (!passwordMatch) throw new Error("Invalid Password");
+          return userFound;
+        } else {
           throw new Error("Invalid Email");
         }
-
-        const passwordMatch = await bcrypt.compare(credentials!.password, userFound.password);
-
-        if (!passwordMatch) throw new Error("Invalid Password");
-
-        return userFound;
       },
     }),
   ],
@@ -48,17 +57,14 @@ export const options: NextAuthOptions = {
         const existingUser = await User.findOne({ email: profile.email });
 
         if (existingUser) {
+          if (!existingUser.image) {
+            await User.findOneAndUpdate({ email: profile.email }, { image: profile.picture }, { new: true });
+
+            return true;
+          }
+          console.log(profile);
           return true;
         }
-
-        const newUser = await User.create({
-          email: profile.email,
-          image: profile.image,
-        });
-
-        await newUser.save();
-
-        return true;
       } catch (error) {
         console.error("Error signing in", error);
         return false;
@@ -67,5 +73,11 @@ export const options: NextAuthOptions = {
     async session({ session, user, token }) {
       return session;
     },
+    async redirect({ url, baseUrl }) {
+      return baseUrl;
+    },
   },
+  session: { strategy: "jwt" },
+  pages: { signIn: "/auth/signin" },
+  secret: process.env.NEXTAUTH_SECRET,
 };
