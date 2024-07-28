417f16bf3478648a5b0ec56d0f00589125168d3e
xortaa <kercwinpaul@gmail.com>
2024-07-28 15:09:00 +0800

    models for organization

---
+++ models/organization.ts
@@ -0,0 +1,54 @@
+import { Schema, model, models } from "mongoose";
+
+const OrganizationSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, "name is required"],
+  },
+  image: {
+    type: String,
+  },
+  affiliation: {
+    type: String,
+  },
+  is_univ_wide: {
+    type: Boolean,
+    required: [true, "is_univ_wide is required"],
+  },
+  website: {
+    type: String,
+  },
+  email: {
+    type: String,
+    required: [true, "email is required"],
+  },
+  socials: [
+    {
+      platform: { type: String },
+      link: { type: String },
+    },
+  ],
+  members: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Member",
+    },
+  ],
+  advisers: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+  accreditation_code: {
+    type: String,
+  },
+  is_active: {
+    type: Boolean,
+    default: true,
+  },
+});
+
+const Organization = models.Organization || model("Organization", OrganizationSchema);
+
+export default Organization;
--- models/user.ts
+++ models/user.ts
@@ -4,7 +4,7 @@ const UserSchema = new Schema({
   email: {
     type: String,
     unique: true,
-    required: true,
+    required: [true, "email is required"],
   },
   password: {
     type: String,
@@ -14,8 +14,87 @@ const UserSchema = new Schema({
   },
   role: {
     type: String,
+    enum: ["RSO", "OSA", "SOCC", "AU", "ADVISER"],
     required: true,
     default: "AU",
+  },
+  first_name: {
+    type: String,
+    required: [true, "First name is required for ADVISER role only"],
+    validate: {
+      validator: function (v: string) {
+        return this.role === "ADVISER" ? v !== "" : true;
+      },
+      message: "First name is required for ADVISER role only",
+    },
+  },
+  middle_name: {
+    type: String,
+    required: [true, "Middle name is required for ADVISER role only"],
+    validate: {
+      validator: function (v: string) {
+        return this.role === "ADVISER" ? v !== "" : true;
+      },
+      message: "Middle name is required for ADVISER role only",
+    },
+  },
+  last_name: {
+    type: String,
+    required: [true, "Last name is required for ADVISER role only"],
+    validate: {
+      validator: function (v: string) {
+        return this.role === "ADVISER" ? v !== "" : true;
+      },
+      message: "Last name is required for ADVISER role only",
+    },
+  },
+  contact_number: {
+    type: String,
+    required: [true, "Contact number is required for ADVISER role only"],
+    validate: {
+      validator: function (v: string) {
+        return this.role === "ADVISER" ? v !== "" : true;
+      },
+      message: "Contact number is required for ADVISER role only",
+    },
+  },
+  college: {
+    type: String,
+    required: [true, "College is required for ADVISER role only"],
+    validate: {
+      validator: function (v: string) {
+        return this.role === "ADVISER" ? v !== "" : true;
+      },
+      message: "College is required for ADVISER role only",
+    },
+  },
+  address: {
+    type: String,
+    required: [true, "Address is required for ADVISER role only"],
+    validate: {
+      validator: function (v: string) {
+        return this.role === "ADVISER" ? v !== "" : true;
+      },
+      message: "Address is required for ADVISER role only",
+    },
+  },
+  second_address: {
+    type: String,
+    validate: {
+      function(v: string) {
+        return this.role !== "ADVISER" || typeof v === "string"; //allow empty string for all roles
+      },
+      message: "Second address is for ADVISER role only",
+    },
+  },
+  signature: {
+    type: String,
+    validate: {
+      function(v: string) {
+        return this.role !== "ADVISER" || typeof v === "string"; //allow empty string for all roles
+      },
+      message: "signature is for ADVISER role only",
+    },
   },
 });
 
