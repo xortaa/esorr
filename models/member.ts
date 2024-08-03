d6b4b7c65c37067c047c4c95fd102c9d94d76598
xortaa <kercwinpaul@gmail.com>
2024-08-04 01:36:53 +0800

    member model edits

--- models/member.ts
+++ models/member.ts
@@ -3,35 +3,27 @@ import { Schema, model, models } from "mongoose";
 const MemberModel = new Schema({
   first_name: {
     type: String,
-    required: [true, "first_name is required"],
   },
   middle_name: {
     type: String,
-    required: [true, "middle_name is required"],
   },
   last_name: {
     type: String,
-    required: [true, "last_name is required"],
   },
   position: {
     type: String,
-    required: [true, "position is required"],
   },
   college: {
     type: String,
-    required: [true, "college is required"],
   },
   student_number: {
     type: String,
-    required: [true, "student_number is required"],
   },
   contact_number: {
     type: String,
-    required: [true, "contact_number is required"],
   },
   address: {
     type: String,
-    required: [true, "address is required"],
   },
   other_organizations: [
     {
@@ -42,31 +34,31 @@ const MemberModel = new Schema({
   ],
   religion: {
     type: String,
-    required: [true, "religion is required"],
   },
   citizenship: {
     type: String,
-    required: [true, "citizenship is required"],
   },
   gender: {
     type: String,
-    required: [true, "gender is required"]
   },
-  educational_background: {
-    type: Schema.Types.ObjectId,
-    ref: "EducationalBackground",
-  },
+  educational_background: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "EducationalBackground",
+    },
+  ],
   is_new_member: {
     type: Boolean,
     default: true,
   },
   program: {
     type: String,
-    required: [true, "program is required"],
   },
   year_level: {
     type: String,
-    required: ["year_level is required", true],
+  },
+  image: {
+    type: String,
   },
 });
 
