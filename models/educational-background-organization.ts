25f7f38b7f00f44f611a1053673835e5a6c8b154
xortaa <kercwinpaul@gmail.com>
2024-08-04 01:37:06 +0800

    educational background organization edits

--- models/educational-background-organization.ts
+++ models/educational-background-organization.ts
@@ -3,11 +3,9 @@ import { Schema, model, models } from "mongoose";
 const EduationalBackgroundOrganizationModel = new Schema({
   name: {
     type: String,
-    required: [true, "name is required"],
   },
   position: {
     type: String,
-    required: [true, "position is required"],
   },
 });
 
