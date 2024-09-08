112a5d4a322212d74e23122759e2badf4e6bdd8d
xortaa <kercwinpaul@gmail.com>
2024-09-08 20:25:17 +0800

    sidebar changes

--- components/SidebarLayoutComponent.tsx
+++ components/SidebarLayoutComponent.tsx
@@ -20,69 +20,60 @@ const SidebarLayoutComponent = ({ children }) => {
             <div className="drawer-content flex flex-col h-full overflow-auto">{children}</div>
             <div className="drawer-side z-0 h-full">
               <label htmlFor="my-drawer-2" aria-label="close sidebar" className="drawer-overlay"></label>
-              <div className="menu bg-base text-base-content h-full w-80 flex-col items-start justify-start gap-2 overflow-auto">
-                <p className="text-lg">Menu</p>
+              <div className="menu bg-base text-base-content h-full w-16 flex-col items-start justify-start">
                 <ul className="w-full">
                   <div className="border-b border-slate-400">
                     {session?.user.role === "RSO" && (
                       <>
-                        <li className="hover:text-white">
+                        <li className="my-6">
                           <Link href="/rso/annexes">
-                            <FileText className="text-primary" />
-                            Annexes
+                            <FileText className="text-base-content hover:text-primary" />
                           </Link>
                         </li>
-                        <li className="hover:text-white">
+                        <li className="my-6">
                           <Link href="/rso/signatories">
-                            <Users className="text-primary" />
-                            Signatory Accounts
+                            <Users className="text-base-content hover:text-primary" />
                           </Link>
                         </li>
                       </>
                     )}
                     {session?.user.role === "OSA" && (
-                      <li className="hover:text-white">
+                      <li className="my-6">
                         <Link href="admin/accounts">
-                          <SquareUserRound className="text-primary" />
-                          Manage Accounts
+                          <SquareUserRound className="text-base-content hover:text-primary" />
                         </Link>
                       </li>
                     )}
                     {session?.user.role && session?.user.role !== "RSO" && (
-                      <li className="hover:text-white">
+                      <li className="my-6">
                         <Link href="admin/organizations">
-                          <Building2 className="text-primary" />
-                          Manage Organizations
+                          <Building2 className="text-base-content hover:text-primary" />
                         </Link>
                       </li>
                     )}
                     {session?.user.role === "OSA" && (
-                      <li className="hover:text-white">
+                      <li className="my-6">
                         <Link href="admin/announcements">
-                          <Megaphone className="text-primary" />
-                          Manage Announcements
+                          <Megaphone className="text-base-content hover:text-primary" />
                         </Link>
                       </li>
                     )}
                   </div>
 
                   <div className="border-b border-slate-400">
-                    <li className="hover:text-white">
+                    <li className="my-6">
                       <Link href="/">
-                        <Megaphone className="text-primary" />
-                        Announcements
+                        <Megaphone className="text-base-content hover:text-primary" />
                       </Link>
                     </li>
-                    <li className="hover:text-white">
+                    <li className="my-6">
                       <Link href="">
-                        <Laptop className="text-primary" />
-                        About
+                        <Laptop className="text-base-content hover:text-primary" />
                       </Link>
                     </li>
-                    <li className="hover:text-white">
+                    <li className="my-6">
                       <Link href="">
-                        <CircleHelp className="text-primary" />
-                        FAQ
+                        <CircleHelp className="text-base-content hover:text-primary" />
                       </Link>
                     </li>
                   </div>
