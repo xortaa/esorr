cd949c8cc43da85d712f1108b711bf3f933b917a
xortaa <kercwinpaul@gmail.com>
2024-09-28 03:04:06 +0800

    changed padding

--- components/PageWrapper.tsx
+++ components/PageWrapper.tsx
@@ -1,4 +1,4 @@
 const PageWrapper = ({ children }) => {
-  return <div className="p-5 pb-20 page_wrapper_height container mx-auto">{children}</div>;
+  return <div className="pt-5 px-20 pb-20 page_wrapper_height container mx-auto">{children}</div>;
 };
 export default PageWrapper;
