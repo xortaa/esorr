12e501ad2c3dc2f07c862211d35f19988248bbbe
xortaa <kercwinpaul@gmail.com>
2024-10-13 12:58:23 +0800

    annex g in pdf list

--- app/pdf/page.tsx
+++ app/pdf/page.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import Annex01 from "@/app/pdf/annex-01/page";
 import Annex02 from "@/app/pdf/annex-02/page";
 import AnnexD from "@/app/pdf/annex-D/page";
+import AnnexG from "@/app/pdf/annex-G/page";
 import AnnexH from "@/app/pdf/annex-H/page";
 import AnnexI from "@/app/pdf/annex-I/page";
 import AnnexJ from "@/app/pdf/annex-J/page";
@@ -20,6 +21,8 @@ const PDFPage = () => {
       <Annex02 />
       <h2 className="text-xl font-bold text-primary">AnnexD</h2>
       <AnnexD />
+      <h2 className="text-xl font-bold text-primary">AnnexG</h2>
+      <AnnexG />
       <h2 className="text-xl font-bold text-primary">AnnexH</h2>
       <AnnexH />
       <h2 className="text-xl font-bold text-primary">AnnexI</h2>
