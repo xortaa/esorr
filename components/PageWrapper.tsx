b6deb59aa4cbc82dda6902f8b7d003f1fb67c630
xortaa <kercwinpaul@gmail.com>
2024-10-14 23:20:12 +0800

    changed pagewrapper container size

--- components/PageWrapper.tsx
+++ components/PageWrapper.tsx
@@ -1,9 +1,5 @@
 const PageWrapper = ({ children }) => {
-  return (
-    <div className="flex page_wrapper_height">
-      <div className="pt-5 lg:px-20 px-5 pb-20 flex-grow">{children}</div>
-    </div>
-  );
+  return <div className="container mx-auto p-4 space-y-8 page_wrapper_height">{children}</div>;
 };
 
 export default PageWrapper;
