5d0729530fb6b0b9f22e7fb7bbfc60e5f40ef0a6
xortaa <kercwinpaul@gmail.com>
2024-10-16 11:04:16 +0800

    follow up routes i forgot to add to the commit

--- app/api/annexes/[organizationId]/annex-a1/route.ts
+++ app/api/annexes/[organizationId]/annex-a1/route.ts
@@ -1,16 +1,15 @@
 import { NextRequest, NextResponse } from "next/server";
 import connectToDatabase from "@/utils/mongodb";
-import AnnexL from "@/models/annex-l";
+import AnnexA1 from "@/models/annex-a1";
 
 export const GET = async (req: NextRequest, { params }: { params: { organizationId: string } }) => {
   await connectToDatabase();
 
   try {
-    const annexL = await AnnexL.find({ organizationId: params.organizationId });
-    return NextResponse.json(annexL, { status: 200 });
+    const annexA1 = await AnnexA1.find({ organizationId: params.organizationId });
+    return NextResponse.json(annexA1, { status: 200 });
   } catch (error) {
     console.error(error);
     return NextResponse.json({ error: "An error occurred" }, { status: 500 });
   }
 };
-  
