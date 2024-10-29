4ae5c3154025bcb50f5acc0a9b966b152f4e1d96
JKTEJADA <josephkiel.tejada.cics@ust.edu.ph>
2024-10-29 17:52:47 +0800

    Annex B Finished, Annex F in progress
    Annex B Completed
    Annex F still in progress

--- app/pdf/annex-B/page.tsx
+++ app/pdf/annex-B/page.tsx
@@ -255,7 +255,7 @@ const MyDocument = () => {
           <Text style={{}}>(as of AY (Insert Year))</Text>
         </View>
 
-        <View style={{ flexDirection: "row", marginTop: 50 }}>
+        <View style={{ flexDirection: "row", marginTop: 30 }}>
           <Text style={{ fontFamily: "Arial Narrow Bold" }}>NAME OF ORGANIZATION</Text>
           <Text style={{ fontFamily: "Arial Narrow Bold", textDecoration: "underline", paddingLeft: 10 }}>
             TEST ORGANIZATION
@@ -285,13 +285,14 @@ const MyDocument = () => {
             <Text style={{ padding: 5, width: "25%", borderRightWidth: 1 }}> Number of Officers</Text>
             <Text style={{ width: "75%" }}></Text>
           </View>
+
+          {/* Age and Gender Distribution of Mmebers */}
           <View style={{ flexDirection: "row", width: "100%", borderTop: 1, textAlign: "left" }}>
             <Text style={{ padding: 5, width: "25%", borderRightWidth: 1 }}>
               Age and Gender
               <Br />
               Distribution of Members
             </Text>
-
             <View style={{ width: "75%", flexDirection: "column" }}>
               <View style={{ width: "100%", flexDirection: "row", textAlign: "center" }}>
                 <Text style={{ width: "20%", borderRightWidth: 1 }}></Text>
@@ -321,6 +322,224 @@ const MyDocument = () => {
               </View>
             </View>
           </View>
+
+          {/* Distribution of Members According to Faculty / College / Institute / School  and Year Level */}
+
+          <View style={{ flexDirection: "row", width: "100%", borderTop: 1, textAlign: "left" }}>
+            <Text style={{ padding: 5, width: "25%", borderRightWidth: 1 }}>
+              Distribution of Members According to Faculty / College / Institute / School and Year Level
+            </Text>
+
+            <View style={{ width: "75%", flexDirection: "column" }}>
+              <View style={{ flexDirection: "row", textAlign: "center" }}>
+                <View style={{ flexDirection: "column", textAlign: "center", width: "35%" }}>
+                  <Text
+                    style={{
+                      padding: 10,
+                      borderRightWidth: 1,
+                      fontFamily: "Arial Narrow Bold",
+                      backgroundColor: "black",
+                      color: "white",
+                    }}
+                  >
+                    INSTITUTE / SCHOOL
+                  </Text>
+                  <Text style={{ textDecoration: "underline", fontSize: 7, padding: 6 }}>
+                    ____________________________
+                  </Text>
+                </View>
+                <View style={{ flexDirection: "column", textAlign: "center", width: "65%", borderLeftWidth: 1 }}>
+                  <Text style={{}}>YEAR LEVEL</Text>
+                  <View style={{ flexDirection: "row", borderTopWidth: 1 }}>
+                    <Text style={{ width: "20%", borderRightWidth: 1, padding: 13 }}>1</Text>
+                    <Text style={{ width: "20%", borderRightWidth: 1, padding: 13 }}>2</Text>
+                    <Text style={{ width: "20%", borderRightWidth: 1, padding: 13 }}>3</Text>
+                    <Text style={{ width: "20%", borderRightWidth: 1, padding: 13 }}>4</Text>
+                    <Text style={{ width: "20%", padding: 13 }}>5</Text>
+                  </View>
+                </View>
+              </View>
+              <View style={{ width: "100%", flexDirection: "row", textAlign: "center", borderTopWidth: 1 }}>
+                <View style={{ flexDirection: "column", textAlign: "center", width: "35%" }}>
+                  <Text
+                    style={{
+                      padding: 13,
+
+                      fontSize: 10,
+                    }}
+                  >
+                    Write Program and Major
+                  </Text>
+                </View>
+                <View style={{ flexDirection: "column", textAlign: "center", width: "65%", borderLeftWidth: 1 }}>
+                  <View style={{ flexDirection: "row" }}>
+                    <View style={{ width: "20%", borderRightWidth: 1, flexDirection: "row" }}>
+                      <Text style={{ width: "50%", borderRightWidth: 1 }}>
+                        N<Br />e<Br />w
+                      </Text>
+                      <Text style={{ width: "50%" }}>
+                        O<Br />l<Br />d
+                      </Text>
+                    </View>
+                    <View style={{ width: "20%", borderRightWidth: 1, flexDirection: "row" }}>
+                      <Text style={{ width: "50%", borderRightWidth: 1 }}>
+                        N<Br />e<Br />w
+                      </Text>
+                      <Text style={{ width: "50%" }}>
+                        O<Br />l<Br />d
+                      </Text>
+                    </View>
+                    <View style={{ width: "20%", borderRightWidth: 1, flexDirection: "row" }}>
+                      <Text style={{ width: "50%", borderRightWidth: 1 }}>
+                        N<Br />e<Br />w
+                      </Text>
+                      <Text style={{ width: "50%" }}>
+                        O<Br />l<Br />d
+                      </Text>
+                    </View>
+                    <View style={{ width: "20%", borderRightWidth: 1, flexDirection: "row" }}>
+                      <Text style={{ width: "50%", borderRightWidth: 1 }}>
+                        N<Br />e<Br />w
+                      </Text>
+                      <Text style={{ width: "50%" }}>
+                        O<Br />l<Br />d
+                      </Text>
+                    </View>
+                    <View style={{ width: "20%", flexDirection: "row" }}>
+                      <Text style={{ width: "50%", borderRightWidth: 1 }}>
+                        N<Br />e<Br />w
+                      </Text>
+                      <Text style={{ width: "50%" }}>
+                        O<Br />l<Br />d
+                      </Text>
+                    </View>
+                  </View>
+                </View>
+              </View>
+              {/* Inputs */}
+              <View>
+                <NumofOfficers indexNum={"1"} facInstName={"Faculty of Pharmacy"} />
+                <NumofOfficers indexNum={"2"} facInstName={"Institute of Physical and Athletics"} />
+                <NumofOfficers indexNum={"3"} facInstName={"Faculty of Pharmacy"} />
+                <NumofOfficers indexNum={"4"} facInstName={"Institute of Physical and Athletics"} />
+                <NumofOfficers indexNum={"5"} facInstName={"Faculty of Pharmacy"} />
+                <NumofOfficers indexNum={"6"} facInstName={"Institute of Physical and Athletics"} />
+                <NumofOfficers indexNum={"7"} facInstName={"Faculty of Pharmacy"} />
+                <NumofOfficers indexNum={"8"} facInstName={"Institute of Physical and Athletics"} />
+                <NumofOfficers indexNum={"9"} facInstName={"Faculty of Pharmacy"} />
+                <NumofOfficers indexNum={"10"} facInstName={"Institute of Physical and Athletics"} />
+                <NumofOfficers indexNum={"11"} facInstName={"Institute of Physical and Athletics"} />
+              </View>
+            </View>
+          </View>
+
+          <View style={{ flexDirection: "row", width: "100%", borderTop: 1 }}>
+            <Text
+              style={{
+                padding: 5,
+                width: "51.4%",
+                borderRightWidth: 1,
+                fontFamily: "Arial Narrow Bold",
+                textAlign: "right",
+              }}
+            >
+              Total Number of MEMBERS
+            </Text>
+
+            <View style={{ width: "48.6%", flexDirection: "column" }}>
+              <View style={{ flexDirection: "row", textAlign: "center" }}>
+                <View style={{ width: "20%", borderRightWidth: 1, flexDirection: "row" }}>
+                  <Text style={{ width: "50%", borderRightWidth: 1, paddingVertical: 10 }}>1</Text>
+                  <Text style={{ width: "50%", paddingVertical: 10 }}>1</Text>
+                </View>
+                <View style={{ width: "20%", borderRightWidth: 1, flexDirection: "row" }}>
+                  <Text style={{ width: "50%", borderRightWidth: 1, paddingVertical: 10 }}>1</Text>
+                  <Text style={{ width: "50%", paddingVertical: 10 }}>1</Text>
+                </View>
+                <View style={{ width: "20%", borderRightWidth: 1, flexDirection: "row" }}>
+                  <Text style={{ width: "50%", borderRightWidth: 1, paddingVertical: 10 }}>1</Text>
+                  <Text style={{ width: "50%", paddingVertical: 10 }}>1</Text>
+                </View>
+                <View style={{ width: "20%", borderRightWidth: 1, flexDirection: "row" }}>
+                  <Text style={{ width: "50%", borderRightWidth: 1, paddingVertical: 10 }}>1</Text>
+                  <Text style={{ width: "50%", paddingVertical: 10 }}>1</Text>
+                </View>
+                <View style={{ width: "20%", flexDirection: "row" }}>
+                  <Text style={{ width: "50%", borderRightWidth: 1, paddingVertical: 10 }}>1</Text>
+                  <Text style={{ width: "50%", paddingVertical: 10 }}>1</Text>
+                </View>
+              </View>
+            </View>
+          </View>
+
+          <View style={{ flexDirection: "row", width: "100%", borderTop: 1, alignItems: "center" }}>
+            <Text style={{ padding: 5, width: "50%" }}></Text>
+            <Text style={{ textAlign: "right", padding: 5, width: "50%", fontFamily: "Arial Narrow Bold" }}>
+              {" "}
+              Total Number of Officers and Members:{" "}
+              <Text style={{ textDecoration: "underline", fontFamily: "Arial Narrow" }}>2000</Text>{" "}
+            </Text>
+          </View>
+        </View>
+
+        {/* Signatories */}
+        <Text style={{ fontFamily: "Times-Bold" }}>Certified By:</Text>
+
+        <View style={{ flexDirection: "row", fontSize: 7, textAlign: "center", marginTop: 50 }}>
+          <View style={{ flexDirection: "column", width: "50%", marginHorizontal: 30 }}>
+            <Text style={{ fontFamily: "Arial Narrow Bold", borderTopWidth: 1 }}>
+              SIGNATURE OVER PRINTED NAME OF SECRETARY
+            </Text>
+          </View>
+          <View style={{ flexDirection: "column", width: "50%", marginHorizontal: 30 }}>
+            <Text style={{ fontFamily: "Arial Narrow Bold", borderTopWidth: 1 }}>
+              SIGNATURE OVER PRINTED NAME OF ADVISER
+            </Text>
+          </View>
+        </View>
+
+        <Text break style={{ marginBottom: 10 }}>
+          LIST OF MEMBERS FOR AY 2021-2022
+        </Text>
+
+        <View style={{ borderWidth: 1, textAlign: "center" }}>
+          <View style={{ flexDirection: "row", width: "100%" }}>
+            <Text style={{ borderRightWidth: 1, width: "5%" }}> </Text>
+            <Text style={{ borderRightWidth: 1, width: "25%" }}> Name </Text>
+            <Text style={{ borderRightWidth: 1, width: "20%" }}> Student Number</Text>
+            <Text style={{ borderRightWidth: 1, width: "25%" }}> Program </Text>
+            <Text style={{ width: "25%" }}> Membership Status </Text>
+          </View>
+
+          <View style={{ flexDirection: "row", width: "100%", borderTopWidth: 1 }}>
+            <Text style={{ borderRightWidth: 1, width: "5%" }}> </Text>
+            <Text style={{ borderRightWidth: 1, width: "25%" }}> </Text>
+            <Text style={{ borderRightWidth: 1, width: "20%" }}> </Text>
+            <Text style={{ borderRightWidth: 1, width: "25%" }}> </Text>
+            <View style={{ width: "25%", flexDirection: "row" }}>
+              <View style={{ width: "50%", borderRightWidth: 1 }}>
+                <Text>
+                  {" "}
+                  <Text style={{ fontFamily: "Boxed" }}> 0 </Text> Old{" "}
+                </Text>
+              </View>
+              <View style={{ width: "50%" }}>
+                <Text>
+                  <Text style={{ fontFamily: "Boxed" }}> 0 </Text> New{" "}
+                </Text>
+              </View>
+            </View>
+          </View>
+
+          {/* Input */}
+
+          <ListofMembers />
+          <ListofMembers />
+          <ListofMembers />
+          <ListofMembers />
+          <ListofMembers />
+          <ListofMembers />
+          <ListofMembers />
         </View>
 
         {/* Section 1 */}
@@ -351,6 +570,68 @@ const generatePDF = async () => {
   const blob = await asPdf.toBlob();
   const url = URL.createObjectURL(blob);
   window.open(url, "_blank");
+};
+
+const ListofMembers = () => {
+  return (
+    <View style={{ flexDirection: "row", width: "100%", borderTopWidth: 1 }}>
+      <Text style={{ borderRightWidth: 1, width: "5%" }}> </Text>
+      <Text style={{ borderRightWidth: 1, width: "25%" }}> </Text>
+      <Text style={{ borderRightWidth: 1, width: "20%" }}> </Text>
+      <Text style={{ borderRightWidth: 1, width: "25%" }}> </Text>
+      <View style={{ width: "25%", flexDirection: "row" }}>
+        <View style={{ width: "50%", borderRightWidth: 1 }}>
+          <Text></Text>
+        </View>
+        <View style={{ width: "50%" }}>
+          <Text></Text>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const NumofOfficers = ({ indexNum, facInstName }) => {
+  return (
+    <View style={{ width: "100%", flexDirection: "row", borderTopWidth: 1 }}>
+      <View style={{ flexDirection: "column", width: "35%" }}>
+        <Text
+          style={{
+            fontSize: 10,
+            paddingLeft: 2,
+            paddingTop: 4,
+            paddingHorizontal: 5,
+          }}
+        >
+          {indexNum}.<Text style={{ textAlign: "left" }}> {facInstName}</Text>
+        </Text>
+      </View>
+      <View style={{ flexDirection: "column", textAlign: "center", width: "65%", borderLeftWidth: 1 }}>
+        <View style={{ flexDirection: "row" }}>
+          <View style={{ width: "20%", borderRightWidth: 1, flexDirection: "row" }}>
+            <Text style={{ width: "50%", borderRightWidth: 1, paddingVertical: 10 }}>1</Text>
+            <Text style={{ width: "50%", paddingVertical: 10 }}>1</Text>
+          </View>
+          <View style={{ width: "20%", borderRightWidth: 1, flexDirection: "row" }}>
+            <Text style={{ width: "50%", borderRightWidth: 1, paddingVertical: 10 }}>1</Text>
+            <Text style={{ width: "50%", paddingVertical: 10 }}>1</Text>
+          </View>
+          <View style={{ width: "20%", borderRightWidth: 1, flexDirection: "row" }}>
+            <Text style={{ width: "50%", borderRightWidth: 1, paddingVertical: 10 }}>1</Text>
+            <Text style={{ width: "50%", paddingVertical: 10 }}>1</Text>
+          </View>
+          <View style={{ width: "20%", borderRightWidth: 1, flexDirection: "row" }}>
+            <Text style={{ width: "50%", borderRightWidth: 1, paddingVertical: 10 }}>1</Text>
+            <Text style={{ width: "50%", paddingVertical: 10 }}>1</Text>
+          </View>
+          <View style={{ width: "20%", flexDirection: "row" }}>
+            <Text style={{ width: "50%", borderRightWidth: 1, paddingVertical: 10 }}>1</Text>
+            <Text style={{ width: "50%", paddingVertical: 10 }}>1</Text>
+          </View>
+        </View>
+      </View>
+    </View>
+  );
 };
 
 const AnnexRow = ({ annexName, annexDescription }) => {
