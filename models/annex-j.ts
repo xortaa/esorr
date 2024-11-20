784cf38c0bada10754d6cc2fcad430c00de768be
xortaa <kercwinpaul@gmail.com>
2024-11-20 23:40:47 +0800

    spacing changes

--- models/annex-j.ts
+++ models/annex-j.ts
@@ -15,40 +15,38 @@ const SignatureSchema = new Schema({
   },
 });
 
-const AnnexJSchema = new Schema(
-  {
-    organization: {
-      type: Schema.Types.ObjectId,
-      ref: "Organization",
-      required: true,
-    },
-    academicYear: {
-      type: String,
-      required: true,
-    },
-    president: SignatureSchema,
-    vicePresident: SignatureSchema,
-    secretary: SignatureSchema,
-    treasurer: SignatureSchema,
-    auditor: SignatureSchema,
-    pro: SignatureSchema,
-    adviser: SignatureSchema,
-    status: {
-      type: String,
-      enum: ["Not Submitted", "Rejected", "For Review", "Approved"],
-      default: "Not Submitted",
-    },
-    soccRemarks: {
-      type: String,
-      default: "",
-    },
-    osaRemarks: {
-      type: String,
-      default: "",
-    },
-    dateSubmitted: Date,
+const AnnexJSchema = new Schema({
+  organization: {
+    type: Schema.Types.ObjectId,
+    ref: "Organization",
+    required: true,
   },
-);
+  academicYear: {
+    type: String,
+    required: true,
+  },
+  president: SignatureSchema,
+  vicePresident: SignatureSchema,
+  secretary: SignatureSchema,
+  treasurer: SignatureSchema,
+  auditor: SignatureSchema,
+  pro: SignatureSchema,
+  adviser: SignatureSchema,
+  status: {
+    type: String,
+    enum: ["Not Submitted", "Rejected", "For Review", "Approved"],
+    default: "Not Submitted",
+  },
+  soccRemarks: {
+    type: String,
+    default: "",
+  },
+  osaRemarks: {
+    type: String,
+    default: "",
+  },
+  dateSubmitted: Date,
+});
 
 const AnnexJ = models.AnnexJ || model("AnnexJ", AnnexJSchema);
 
