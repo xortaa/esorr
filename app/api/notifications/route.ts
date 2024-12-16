017a0f0f2cd34eda59e87642cc61fb15acb598b6
xortaa <kercwinpaul@gmail.com>
2024-12-16 11:50:23 +0800

    added dunamic and revalidatie pragma no cach expires 0

--- app/api/notifications/route.ts
+++ app/api/notifications/route.ts
@@ -2,15 +2,20 @@ import connectToDatabase from "@/utils/mongodb";
 import { NextRequest, NextResponse } from "next/server";
 import Notification from "@/models/notification";
 
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 export async function GET(request: NextRequest) {
   try {
     await connectToDatabase();
 
-    const notifications = await Notification.find().sort({ date: -1 });
+    const notifications = await Notification.find().sort({ date: -1 }).lean();
 
     // Set cache control headers
     const headers = new Headers();
-    headers.set("Cache-Control", "no-store, max-age=0");
+    headers.set("Cache-Control", "no-store, no-cache, must-revalidate, max-age=0");
+    headers.set("Pragma", "no-cache");
+    headers.set("Expires", "0");
 
     return NextResponse.json(notifications, {
       headers: headers,
